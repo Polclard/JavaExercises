bc779f507394958c3a17510686886a63ce4ea02a
Polclard <alenjang007@gmail.com>
2023-04-03 20:49:31 +0200

    import SemaphoreClass.java

---
+++ src/OS/SemaphoreClass.java
@@ -0,0 +1,92 @@
+package OS;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreClass {
+
+    static class Thread1 extends Thread
+    {
+        @Override
+        public void run() {
+            try {
+                A();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                A();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            B();
+            try {
+                C();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                C();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Zavrsi Thread1");
+        }
+    }
+
+
+    static class Thread2 extends Thread
+    {
+        @Override
+        public void run() {
+            try {
+                A();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                A();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            B();
+            try {
+                C();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                C();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            B();
+            System.out.println("Zavrsi Thread2");
+        }
+    }
+
+    static Semaphore semaphore = new Semaphore(5);
+    static void A() throws InterruptedException {
+        semaphore.acquire();
+        System.out.println("A");
+    }
+    static void B()
+    {
+        semaphore.release();
+        System.out.println("B");
+    }
+    static void C() throws InterruptedException {
+        semaphore.acquire();
+        System.out.println("C");
+    }
+
+    public static void main(String[] args) {
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+
+        thread1.start();
+        thread2.start();
+
+    }
+
+
+}
